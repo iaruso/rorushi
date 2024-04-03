6e907caf59574cc32d9660b92b5a15f4e22e4007
Yaraslau <iaruso.me@gmail.com>
2024-04-03 21:21:24 +0100

    Added vercel and vite settings

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import compression from 'vite-plugin-compression2'
+import preload from 'vite-plugin-preload'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: './',
+  plugins: [
+    react(),
+    compression(),
+    preload()
+  ],
   server: {
     host: true,
+  },
+	root: 'src/',
+	publicDir: '../public/',
+	build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: false
   }
-})
+});
